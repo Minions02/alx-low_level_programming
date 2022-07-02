d48d743a4d276ab7945e6424ff73f9b882dd7891
Minions02 <adeneyo4jchrist@gmail.com>
2022-07-01 17:19:22 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet using ascii values.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter != '{')
+	{
+		putchar (letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter != '[')
+	{
+		putchar (letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+
