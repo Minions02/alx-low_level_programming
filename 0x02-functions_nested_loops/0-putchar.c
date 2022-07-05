5363b88ce37b0e7348d79351c304098037886cee
Minions02 <adeneyo4jchrist@gmail.com>
2022-07-05 15:50:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet
+ * main - prints _putchar
  *
- * Return: void
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
-void print_alphabet(void)
-{
-	char letter = 'a';
 
-	while (letter <= 'z')
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(letter);
-		letter++;
+		_putchar(text[i]);
 	}
 	_putchar('\n');
+
+	return (0);
 }
