f39e9c540287a659f73dea2744a9f76cedbd24c7
Minions02 <adeneyo4jchrist@gmail.com>
2022-07-05 17:42:50 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,28 +11,22 @@
 
 void print_times_table(int n)
 {int main(void)
-	int row, column, product;
+int row, column, product;
 
-	if (n >= 0 && n < 15)
-	{
-		for (row = 0; row <= n; row++)
-		{
-			for (column = 0; column <= n; column++)
-			{
-				product = row * column;
-
-				if (column == 0)
-					_putchar('0');
-				else if (product < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product % 10 + '0');
-				}
-				else if (product >= 10 && product < 100)
-				{
+if (n >= 0 && n < 15)
+{for (row = 0; row <= n; row++)
+{for (column = 0; column <= n; column++)
+{product = row * column;
+if (column == 0)
+_putchar('0');
+else if (product < 10)
+{_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(product % 10 + '0');}
+else if (product >= 10 && product < 100)
+{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
