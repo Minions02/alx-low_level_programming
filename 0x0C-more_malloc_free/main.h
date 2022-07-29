f68191144f5a732e598331927da711eefe51deba
Minions02 <adeneyo4jchrist@gmail.com>
2022-07-28 17:19:02 -0700

    new

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,10 @@
 #define _MAIN_H_
 
 void *malloc_checked(unsigned int b);
+void _puts(char *str);
+int _putchar(char c);
+int _atoi(const chat *s);
+void print_int(unsigned long int n);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
